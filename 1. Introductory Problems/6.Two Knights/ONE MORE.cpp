17c22dce0b2526df010b6f9d2a216cd67ffebfbd
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-05-28 02:37:19 +0600

    AC

---
+++ 1. Introductory Problems/6.Two Knights/ONE MORE.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+ 
+    for(int k = 1; k <= n; k++) {
+        long double tot = 0;
+ 
+        int h = k * k;
+
+        tot = (h) * ((h - 1) / 2.0);
+
+        tot -= ((k - 2) * 10) + (4) + ((k - 4) * (k - 2) * 4) + ((k - 4) * 2);
+
+        cout << fixed << (long long) tot << "\n";
+    }
+    return 0;
+}
