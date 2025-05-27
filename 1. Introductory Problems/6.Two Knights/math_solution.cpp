469ef514060a419d49f7f371d49801cb607dedb9
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-05-28 02:22:06 +0600

    Accepted!

--- 1. Introductory Problems/6.Two Knights/math_solution.cpp
+++ 1. Introductory Problems/6.Two Knights/math_solution.cpp
@@ -5,9 +5,9 @@ all Formula:
     For K Knights Moves = (i * i)Ck
  
     for 2knights Move = (i*i)C2 = i * i * (i * i - 1) / 2
-    Attacking Move = 2[horizontal 2x3 grid]*2[vertical 2x3 grid]*(i-1)*(i-2)
+    Attacking Move = 2[horizontal 2x3 grid] * 2[vertical 2x3 grid] * (i - 1) * (i - 2)
  
-    ans = i^2*(i^2-1)/2 - 4*(i-1)*(i-2) [2x3 and 3x2 grid]
+    ans = i^2 * (i^2 - 1) / 2 - 4 * (i - 1) * (i - 2)
 */
 #include<bits/stdc++.h>
 #define int long long
