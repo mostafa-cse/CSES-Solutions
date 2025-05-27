7e152007002c19d019fce3604444743bbd2196ed
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-05-28 02:18:47 +0600

    Accepted

---
+++ 1. Introductory Problems/6.Two Knights/math_solution.cpp
@@ -0,0 +1,34 @@
+/*
+all Formula:
+    For First Knight Move = i * i
+    for Second knight Moves = (i * i) - 1
+    For K Knights Moves = (i * i)Ck
+ 
+    for 2knights Move = (i*i)C2 = i * i * (i * i - 1) / 2
+    Attacking Move = 2[horizontal 2x3 grid]*2[vertical 2x3 grid]*(i-1)*(i-2)
+ 
+    ans = i^2*(i^2-1)/2 - 4*(i-1)*(i-2) [2x3 and 3x2 grid]
+*/
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+#define all(x) x.begin(), x.end()
+string cdn[2] = {"YES", "NO"};
+#ifndef ONLINE_JUDGE
+#define dout(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define dout(...)
+#endif
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);  cout.tie(0);
+    int n;
+    cin >> n;
+ 
+    for (int i = 1; i <= n; ++i) {
+        int total = i * i * (i * i - 1) / 2;
+        int attacking = 4 * (i - 1) * (i - 2);
+ 
+        cout << total - attacking << endl;
+    }
+    return 0;
