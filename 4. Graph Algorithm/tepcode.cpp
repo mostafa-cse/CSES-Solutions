28b5171a0369686056420b29f430288b4f8aabb1
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-07-18 07:39:05 +0600

    Accepted!

--- 4. Graph Algorithm/tepcode.cpp
+++ 4. Graph Algorithm/tepcode.cpp
@@ -1,88 +1,156 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define max3(a, b, c) max(max(a, b), c)
+#define max4(a, b, c, d) max(max(a, b), max(c, d))
+#define fr(i, n) for (ll i = 0; i < n; i++)
+ll gcd(ll a, ll b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+class SCCmaker
+{
+    vector<vector<ll>> DAG;
+    vector<ll> info;
+    ll number_of_nodes;
+
+    void dfs(ll node, vector<vector<ll>> &adj, vector<ll> &vis, vector<ll> &order)
+    {
+        vis[node] = 1;
+        for (auto &i : adj[node])
+        {
+            if (vis[i] != 1)
+            {
+                dfs(i, adj, vis, order);
+            }
+        }
+        order.push_back(node);
+    }
+    void dfs2(ll node, vector<vector<ll>> &revadj, vector<ll> &temp, vector<ll> &vis)
+    {
+        vis[node] = 1;
+        temp.push_back(node);
+        for (auto &i : revadj[node])
+        {
+            if (vis[i] == 0)
+            {
+                dfs2(i, revadj, temp, vis);
+            }
+        }
+    }
+
+public:
+    SCCmaker(vector<vector<ll>> &adj, vector<ll> &information, ll (*combine)(vector<ll> &temp, vector<ll> &information), vector<pair<ll, ll>> &edges, ll n)
+    {
+        vector<ll> order;
+        vector<ll> vis(n + 1, 0);
+        for (ll i = 1; i <= n; i++)
+        {
+            if (!vis[i])
+                dfs(i, adj, vis, order);
+        }
+        reverse(order.begin(), order.end());
+        vector<ll> vis2(n + 1, 0);
+        vector<vector<ll>> revadj(n + 1);
+        for (auto &i : edges)
+        {
+            revadj[i.second].push_back(i.first);
+        }
+        vector<ll> parentset(n + 1, 0);
+        ll c = 0;
+        for (auto &i : order)
+        {
+            if (!vis2[i])
+            {
+                vector<ll> temp;
+                dfs2(i, revadj, temp, vis2);
+                ll z = temp.size();
+                for (ll j = 0; j < z; j++)
+                {
+                    parentset[temp[j]] = c;
+                }
+                ll sum = combine(temp, information);
+                info.push_back(sum);
+                c++;
+            }
+        }
+        DAG.resize(c);
+        number_of_nodes = c;
+        for (auto &i : edges)
+        {
+            if (parentset[i.first] != parentset[i.second])
+            {
+                DAG[parentset[i.first]].push_back(parentset[i.second]);
+            }
+        }
+    }
+    vector<vector<ll>> getDAG()
+    {
+        return DAG;
+    }
+    vector<ll> getinfo()
+    {
+        return info;
+    }
+    ll get_numberofnodes()
+    {
+        return number_of_nodes;
+    }
+};
+ll combine(vector<ll> &temp, vector<ll> &information)
+{
+    ll sum = 0;
+    for (auto &i : temp)
+    {
+        sum += information[i - 1];
+    }
+    return sum;
+}
+ll solve(ll node, vector<vector<ll>> &DAG, vector<ll> &DAGinfo, vector<ll> &dp)
+{
+    ll res = 0;
+    if (dp[node] != -1)
+        return dp[node];
+    for (auto &i : DAG[node])
+    {
+        res = max(res, solve(i, DAG, DAGinfo, dp));
+    }
+    return dp[node] = (res + DAGinfo[node]);
+}
 int main()
 {
-    int n, m;
+    fastio;
+    ll n, m;
     cin >> n >> m;
-    vector<int> g1[2 * m + 1], g2[2 * m + 1];
-    for (int i = 0; i < n; i++)
+    vector<ll> val(n);
+    for (auto &i : val)
     {
-        char x, y;
-        int a, b;
-        cin >> x >> a >> y >> b;
-
-        if (x == '-')
-            a = 2 * m - a + 1;
-        if (y == '-')
-            b = 2 * m - b + 1;
-
-        g1[2 * m - a + 1].push_back(b);
-        g1[2 * m - b + 1].push_back(a);
-
-        g2[a].push_back(2 * m - b + 1);
-        g2[b].push_back(2 * m - a + 1);
+        cin >> i;
     }
-
-    stack<int> order;
-    vector<int> vis(2 * n + 1, 0);
-    function<void(int)> dfs = [&](int u) -> void
+    vector<vector<ll>> adj(n + 1);
+    vector<pair<ll, ll>> edges;
+    for (ll i = 1; i <= m; i++)
     {
-        vis[u] = 1;
-        for (int v : g1[u])
-        {
-            if (vis[v] == 0)
-            {
-                dfs(v);
-            }
-        }
-        order.push(u);
-    };
-    for (int i = 1; i <= 2 * m; ++i)
-    {
-        if (vis[i] == 0)
-        {
-            dfs(i);
-        }
+        ll x, y;
+        cin >> x >> y;
+        edges.push_back({x, y});
+        adj[x].push_back(y);
     }
-
-    fill(vis.begin(), vis.end(), 0);
-    function<void(int, int)> dfs2 = [&](int u, int clr) -> void
+    SCCmaker sccmaker(adj, val, combine, edges, n);
+    ll DAGnodes = sccmaker.get_numberofnodes();
+    vector<vector<ll>> DAG = sccmaker.getDAG();
+    vector<ll> DAGinfo = sccmaker.getinfo();
+    vector<ll> dp(DAGnodes + 1, -1);
+    ll res = 0;
+    for (ll i = 0; i < DAGnodes; i++)
     {
-        vis[u] = clr;
-        for (int v : g2[u])
-        {
-            if (vis[v] == 0)
-            {
-                dfs2(v, clr);
-            }
-        }
-    };
-
-    int k = 0;
-    while (!order.empty())
-    {
-        int x = order.top();
-        if (!vis[x])
-        {
-            ++k;
-            dfs2(x, k);
-        }
-        order.pop();
+        res = max(res, solve(i, DAG, DAGinfo, dp));
     }
-
-    vector<char> ans(n + 1, ' ');
-    for (int i = 1; i <= m; i++)
-    {
-        if (vis[i] == vis[2 * m - i + 1])
-        {
-            cout << "IMPOSSIBLE\n";
-            return 0;
-        }
-        ans[i] = (vis[i] > vis[2 * m - i + 1]);
-    }
-    for (int i = 1; i <= m; i++)
-    {
-        cout << ((ans[i]) ? '+' : '-') << " ";
-    }
-    return 0;
+    cout << res << "\n";
 }
