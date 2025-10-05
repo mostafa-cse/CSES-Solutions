6585e5c0cd943a26067e18f64611846392fa2b16
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-10-05 22:21:46 +0600

    Accepeted

--- 5. Range Queries/test.cpp
+++ 5. Range Queries/test.cpp
@@ -1,81 +1,110 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
-struct Node {
-    int val;
-    Node *left, *right;
-    Node (int val = 0, Node *left = nullptr, Node *right = nullptr) {
-        this->val = val;
-        this->left = left;
-        this->right = right;
+#define int long long
+
+class PersistentSegmentTree {
+private:
+    struct node {
+        int x = 0;
+        int l = 0, r = 0;
+        node() {};
+    };
+
+    int new_node() {
+        st.push_back(node());
+        return (int)st.size() - 1;
     }
-};
-struct PST {
-    int n;
-    vector<int> a;
-    PST(int n = 0) : n(n), a(n + 1) {}
-    Node *build(int l, int r) {
-        if (l == r) {
-            return new Node(a[l]);
+
+    int build(int L, int R) {
+        int p = new_node();
+        if (L == R) {
+            st[p].x = A[L];
+            return p;
         }
-        int mid = (l + r) >> 1;
-        Node *leftChild = build(l, mid);
-        Node *rightChild = build(mid + 1, r);
-        return new Node(leftChild->val + rightChild->val, leftChild, rightChild);
+
+        int m = (L + R) >> 1;
+        int lc = build(L, m);
+        int rc = build(m + 1, R);
+        st[p].l = lc;
+        st[p].r = rc;
+        st[p].x = st[lc].x + st[rc].x;
+        return p;
     }
-    Node *update(Node *prev, int l, int r, int idx, int val) {
-        if (l == r) {
-            return new Node(val);
+
+    int upd(int prev, int L, int R, int i, int val) {
+        int p = new_node();
+        st[p] = st[prev];
+        if (L == R) {
+            st[p].x = val;
+            return p;
         }
-        int mid = (l + r) >> 1;
-        if (idx <= mid) {
-            Node *newLeft = update(prev->left, l, mid, idx, val);
-            return new Node(newLeft->val + prev->right->val, newLeft, prev->right);
+        int m = (L + R) >> 1;
+        if (i <= m) {
+            st[p].l = upd(st[prev].l, L, m, i, val);
         } else {
-            Node *newRight = update(prev->right, mid + 1, r, idx, val);
-            return new Node(prev->left->val + newRight->val, prev->left, newRight);
+            st[p].r = upd(st[prev].r, m + 1, R, i, val);
         }
+        st[p].x = st[st[p].l].x + st[st[p].r].x;
+        return p;
     }
-    int query(Node *node, int l, int r, int ql, int qr) {
-        if (node == nullptr || l > qr || r < ql) {
+
+    int query(int p, int L, int R, int i, int j) {
+        if (!p || j < L || R < i) {
             return 0;
         }
-        if (ql <= l && r <= qr) {
-            return node->val;
+        if (i <= L && R <= j) {
+            return st[p].x;
         }
-        int mid = (l + r) >> 1;
-        return query(node->left, l, mid, ql, qr) + query(node->right, mid + 1, r, ql, qr);
+        int m = (L + R) >> 1;
+        int left = query(st[p].l, L, m, i, j);
+        int right = query(st[p].r, m + 1, R, i, j);
+        return left + right;
     }
-};
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
+public:
+    int n;
+    vector<int> A;
+    vector<node> st;
+    PersistentSegmentTree(int sz) : n(sz), A(n + 1) {
+        st.reserve((size_t)n * 25 + 10);
+        st.push_back(node());
+    }
+    int build() { return build(1, n); }
+    int upd(int root, int i, int val) { return upd(root, 1, n, i, val); }
+    int query(int root, int i, int j) { return query(root, 1, n, i, j); }
+};
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
     int n, q;
     cin >> n >> q;
-
-    PST st(n);
-    for (int i = 1; i <= n; i++) {
-        cin >> st.a[i];
+    PersistentSegmentTree st(n);
+    for (int i = (1); i < (n + 1); i++) {
+        cin >> st.A[i];
     }
-    Node *root = st.build(1, n);
 
-    // Allocate enough space for all versions: initial + up to q new versions
-    vector<Node *> pointer(q + 5, root);
+    int root0 = st.build();
+    vector<int> root;
+    root.push_back(0); // make 1-Based
+    root.push_back(root0);
 
-    int p = 2;
-    for (int i = 1; i <= q; i++) {
-        int qt;
-        cin >> qt;
-        if (qt == 1) {
-            int k, a, x; cin >> k >> a >> x;
-            pointer[k] = st.update(pointer[k], 1, n, a, x);
-        } else if (qt == 2) {
-            int k, a, b; cin >> k >> a >> b;
-            cout << st.query(pointer[k], 1, n, a, b) << '\n';
-        } else {
-            int k; cin >> k;
-            pointer[p++] = pointer[k];
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int k, a, x;
+            cin >> k >> a >> x;
+            int newRoot = st.upd(root[k], a, x);
+            root[k] = newRoot;
+        } else if (t == 2) {
+            int k, a, b;
+            cin >> k >> a >> b;
+            cout << st.query(root[k], a, b) << endl;
+        } else { // t == 3
+            int k;
+            cin >> k;
+            root.push_back(root[(int)k]);
         }
     }
     return 0;
